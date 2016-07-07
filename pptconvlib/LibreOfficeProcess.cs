25b29835b47ee573b0a2d6a8e929ba89a44731f8
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-06 22:01:45 -0700

    checked in a single-threaded converter impl

---
+++ pptconvlib/LibreOfficeProcess.cs
@@ -0,0 +1,76 @@
+﻿namespace pptconv
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Diagnostics;
+
+    public class LibreOfficeProcess
+    {
+        private readonly LibreOfficeFinder finder;
+
+        public LibreOfficeProcess(LibreOfficeFinder sofficeFinder)
+        {
+            finder = sofficeFinder;
+        }
+
+        public bool Convert(string presentation, string outdir, TimeSpan timeout)
+        {
+            if (finder == null || !finder.Found)
+                return false;
+
+            if (!File.Exists(presentation))
+                return false;
+
+            if (!Directory.Exists(outdir))
+                return false;
+
+            string output = Path.Combine(outdir, Path.GetFileName(presentation));
+
+            if (File.Exists(output))
+                File.Delete(output);
+
+            if (IsRunning)
+                ProcessHelper.TaskKill("soffice.bin");
+
+            if (IsRunning)
+                return false;
+
+            string[] arguments = new string[]
+            {
+                "--nologo",
+                "--headless",
+                "--invisible",
+                "--norestore",
+                "--nodefault",
+                "--nocrashreport",
+                "--nofirststartwizard",
+                string.Format("--outdir \"{0}\"", outdir),
+                string.Format("--convert-to pdf \"{0}\"", presentation)
+            };
+
+            ProcessStartInfo sinfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                FileName = finder.SOfficeBinaryPath,
+                Arguments = string.Join(" ", arguments),
+                WorkingDirectory = Path.GetDirectoryName(finder.SOfficeBinaryPath),
+            };
+
+            using (Process sofficeProcess = new Process { StartInfo = sinfo })
+            {
+                sofficeProcess.Start();
+                sofficeProcess.WaitForExit((int)timeout.TotalMilliseconds);
+            }
+
+            Thread.Sleep(75);
+            return File.Exists(output);
+        }
+
+        public bool IsRunning
+        {
+            get { return Process.GetProcessesByName("soffice.bin").Count() > 0; }
+        }
+    }
+}
---
+++ pptconvlib/ProcessHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace pptconv
+{
+    using System.Diagnostics;
+
+    internal class ProcessHelper
+    {
+        public static void ExecuteScript(string script_path, string command_line = null, bool headless = false)
+        {
+            using (Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = script_path,
+                    Arguments = command_line,
+                    CreateNoWindow = headless,
+                    UseShellExecute = true,
+                }
+            })
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        public static void TaskKill(string name)
+        {
+            ExecuteScript("taskkill", string.Format("/F /T /IM \"{0}\"", name), true);
+        }
+    }
+}
