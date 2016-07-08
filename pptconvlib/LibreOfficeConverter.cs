7239d059318e45886edb72702985560e5795daf8
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-07 17:17:35 -0700

    logic fixes in Converter class

--- pptconvlib/LibreOfficeConverter.cs
+++ pptconvlib/LibreOfficeConverter.cs
@@ -13,6 +13,7 @@
 
         private Thread convertThread;
         private readonly string outdir;
+        private TimeSpan maxJoinTimeout;
         private ManualResetEventSlim resetEvent;
         private readonly LibreOfficeFinder finder;
         private readonly LibreOfficeProcess officeProcess;
@@ -22,21 +23,31 @@
         {
             finder = officeFinder;
             outdir = outputDirictory;
-            queue = new BlockingCollection<ConvertData>();
             officeProcess = new LibreOfficeProcess(finder);
+            queue = new BlockingCollection<ConvertData>();
             convertThread = new Thread(ConvertThreadMain);
             resetEvent = new ManualResetEventSlim(false);
+            maxJoinTimeout = TimeSpan.FromSeconds(10);
             convertThread.Start();
         }
 
         public LibreOfficeConverter()
             : this(new LibreOfficeFinder(), Directory.GetCurrentDirectory())
-        { }
+        {
+            finder.Search();
+        }
 
         public void Queue(string slides, TimeSpan timeout)
         {
-            if (IsDisposed && ConversionFailed != null)
-                ConversionFailed(slides);
+            if (IsDisposed)
+            {
+                if (ConversionFailed != null)
+                    ConversionFailed(slides);
+                return;
+            }
+
+            if (maxJoinTimeout < timeout)
+                maxJoinTimeout = timeout;
 
             queue.Add(new Tuple<string, TimeSpan>(slides, timeout));
 
@@ -44,24 +55,35 @@
                 resetEvent.Set();
         }
 
+        public void Flush()
+        {
+            lock(this)
+            {
+                while (!queue.IsCompleted && queue.Count > 0)
+                {
+                    if (queue.IsAddingCompleted)
+                        return;
+                    else
+                    {
+                        ConvertData data = queue.Take();
+
+                        if (officeProcess.Convert(data.Item1, outdir, data.Item2))
+                        {
+                            if (ConversionSucceed != null) ConversionSucceed(data.Item1);
+                            else if (ConversionFailed != null) ConversionFailed(data.Item1);
+                        }
+                    }
+                }
+            }
+        }
+
         private void ConvertThreadMain()
         {
             while (!queue.IsAddingCompleted)
             {
                 resetEvent.Wait();
-
-                while(!queue.IsCompleted)
-                {
-                    var data = queue.Take();
-
-                    if (officeProcess.Convert(data.Item1, outdir, data.Item2))
-                        if (ConversionSucceed != null) ConversionSucceed(data.Item1);
-                    else
-                        if (ConversionFailed != null) ConversionFailed(data.Item1);
-                }
-
-                if (resetEvent.IsSet)
-                    resetEvent.Reset();
+                Flush();
+                resetEvent.Reset();
             }
         }
 
@@ -78,7 +100,10 @@
                 {
                     queue.CompleteAdding();
                     resetEvent.Set();
-                    convertThread.Join();
+                    convertThread.Join(maxJoinTimeout);
+
+                    if (convertThread.IsAlive)
+                        convertThread.Abort();
 
                     resetEvent.Dispose();
                     queue.Dispose();
