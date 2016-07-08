dea77b19b04ed9405badd05a1667b278277d85f4
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-07 17:29:36 -0700

    Added batch processing tests

--- pptconvtest/LibreOfficeConverterTest.cs
+++ pptconvtest/LibreOfficeConverterTest.cs
@@ -2,6 +2,11 @@
 {
     using pptconv;
 
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Collections.Generic;
+
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -44,6 +49,64 @@
 
             Assert.IsTrue(done);
             Assert.IsTrue(failed);
+        }
+
+        [TestMethod]
+        public void BatchQueueFlushTest()
+        {
+            int count = 5;
+            int converted = 0;
+            List<string> batch = new List<string>();
+
+            string testDir = Path.GetDirectoryName(LibreOfficeProcessTest.Slides);
+
+            for (int i = 0; i < count; ++i)
+            {
+                string name = Path.Combine(testDir, string.Format("test_{0}.pptx", i));
+                if (!File.Exists(name))
+                    File.WriteAllBytes(name, Properties.Resources.test);
+                batch.Add(name);
+            }
+
+            using (LibreOfficeConverter converter = new LibreOfficeConverter())
+            {
+                converter.ConversionSucceed += (file) => { converted++; };
+                batch.ForEach(name => { converter.Queue(name, LibreOfficeProcessTest.TestTimeout); });
+                converter.Flush();
+            }
+
+            Assert.AreEqual(converted, count);
+        }
+
+        [TestMethod]
+        public void BatchQueueWaitTest()
+        {
+            int count = 5;
+            int converted = 0;
+            List<string> batch = new List<string>();
+
+            int seconds = 5;
+            TimeSpan timeout = TimeSpan.FromSeconds(seconds);
+            TimeSpan waittime = TimeSpan.FromSeconds(count * seconds);
+
+            string testDir = Path.GetDirectoryName(LibreOfficeProcessTest.Slides);
+
+            for (int i = 0; i < count; ++i)
+            {
+                string name = Path.Combine(testDir, string.Format("test_{0}.pptx", i));
+                if (!File.Exists(name))
+                    File.WriteAllBytes(name, Properties.Resources.test);
+                batch.Add(name);
+            }
+
+            using (LibreOfficeConverter converter = new LibreOfficeConverter())
+            {
+                converter.ConversionSucceed += (file) => { converted++; };
+                batch.ForEach(name => { converter.Queue(name, LibreOfficeProcessTest.TestTimeout); });
+                Thread.Sleep(waittime);
+            }
+
+            Assert.AreEqual(converted, count);
         }
     }
 }
